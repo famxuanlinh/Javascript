aad4d7e66b7752fb2c9408f55a4397884ebc38e7
Linh Fam <famxuanlinh@gmail.com>
2022-01-01 22:24:44 +0700

    Array

---
+++ 2.js
@@ -0,0 +1,6 @@
+var diem = 9;
+if (diem >= 4 && diem < 7) {console.log('Trung binh')}
+else if (diem >= 7 && diem <9 ) {console.log ('Kha')}
+else if (diem >=9 && diem<= 10) {console.log ('Gioi')};
+//Lenh if else
+// || or
---
+++ 3.js
@@ -0,0 +1,12 @@
+// Cach khai bao mang 
+// Cach so 1
+var hv = ["Tai", "Dong", "Thanh"];
+console.log (hv[2]);
+// Cach so 2 (Khai bao trong truoc sau do dua tung mang vao)
+var hv2 = [];
+hv2[0]="Lam";
+hv2[1]="Hung";
+hv2[2]="Viet";
+// Cach so 3 dung tu khoa NEW 
+var hv3 = new Array ('Viet', 'Hung', 'Lam');
+ console.log (hv3[2]);
