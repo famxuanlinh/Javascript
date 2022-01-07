f05caf3d9ead94cc88bdfd7b3bfa059990f7ad8d
Linh Fam <famxuanlinh@gmail.com>
2022-01-07 19:24:15 +0700

    tinh toan

--- 7.js
+++ 7.js
@@ -5,9 +5,42 @@
 //     // End Biết số thành chữ
 
 
-var so1 = "10";
-    so1 = Number(so1);
-    console.log (so1 + 2);
-    //End Biến chữ thành số
+// var so1 = "10";
+//     so1 = Number(so1);
+//     console.log (so1 + 2);
+//     //End Biến chữ thành số
 
+
+// function tinhtong(x1,x2){
+//     Tong = x1 + x2;
+//     console.log ("Tong là = " + Tong)
+// }
+// tinhtong(3,4);
+
+
+// function chao() {
+//     console.log ("Xin chao");
+//     console.log ("Hello");
+// }
+// chao()
+
+// anonymous funtion (Ẩn danh)
+// var z = function (x,y){
+//     return (x+y)/2;
+// }
+//  console.log (z(3,4));
+
+var a=5;
+var b= 10;
+    a++ * b-- + --a * ++b;
+//   5  * b-- + --a * ++b; // 6
+//   5  * 10  +  5  * ++b; // a=5 b=9
+//   5  * 10  + 5   *  10;
+/**
+ * Arithmetic Operators
+ * Các phép toán số học trong lập trình
+ * 1. ++ (increment) -- (decrement)
+ * 2. * / %
+ * 3. + -
+ */
 
