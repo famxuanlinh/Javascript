81fa46a737cea4c301ff05a6ea941a31c5f62490
Linh Fam <famxuanlinh@gmail.com>
2022-01-08 18:11:25 +0700

    Array map & filter

--- Bai21.js
+++ Bai21.js
@@ -1,23 +1,42 @@
 var users = [
-    {name: 'Tom', gender: 'male'},
-    {name: 'Maria', gender: 'female'},
-    {name: 'Jason', gender: 'male'},
-    {name: 'Sora', gender: 'female'},
-    {name: 'Teo', gender: 'male'},
+    { name: 'Tom', gender: 'male' },
+    { name: 'Maria', gender: 'female' },
+    { name: 'Jason', gender: 'male' },
+    { name: 'Sora', gender: 'female' },
+    { name: 'Teo', gender: 'male' },
 ];
 
-var userList = $('#userList');
-var genderFilter = $('#genderFilter');
+var userListElement = $('#userList');
+var genderFilterElement = $('#genderFilter');
 
-render(userList, users);
 
-genderFilter.on ('change', function(){
-    var selectGender = this.value; //male or female
-    //write code here
+genderFilterElement.on('change', function () {
+    var selectedGender = this.value; //male or female
+    console.log(selectedGender);
+    if (selectedGender == "all") {
+        console.log('');
+        render(userListElement, users);
+    }
+    else {
+
+
+        //write code here
+        var filteredUsers = users.filter(function (user) {
+            return user.gender === selectedGender;
+        });
+        console.log(filteredUsers);
+        render(userListElement, filteredUsers);
+    }
 });
 
-function render(container, items) {
+
+function render(parentElement, items) {
     // write code here
+    var htmlItems = items.map(function (item) {
+        return '<li class="list-group-item">' + item.name + '</li>';
+    });
+    var htmlText = htmlItems.join('');
+    parentElement.html(htmlText);
 }
 
-
+render(userListElement, users);
